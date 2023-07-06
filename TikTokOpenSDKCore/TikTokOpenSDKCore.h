4c3d3fec9a96d5b0d6256267b3afc936a4fd5cdf
Marcus Eckert <marcus@MacStudonMarcus.fritz.box>
2023-07-06 14:18:03 +0200

    update

---
+++ TikTokOpenSDKCore/TikTokOpenSDKCore.h
@@ -0,0 +1,18 @@
+//
+//  TikTokOpenSDKCore.h
+//  TikTokOpenSDKCore
+//
+//  Created by Marcus Eckert on 06.07.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TikTokOpenSDKCore.
+FOUNDATION_EXPORT double TikTokOpenSDKCoreVersionNumber;
+
+//! Project version string for TikTokOpenSDKCore.
+FOUNDATION_EXPORT const unsigned char TikTokOpenSDKCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TikTokOpenSDKCore/PublicHeader.h>
+
+
